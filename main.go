dd92ecefb62a920eebac9531462e0a105364ad14
Abdullah Zakariyya <66228619+Abdillahzakkie@users.noreply.github.com>
2022-06-04 15:55:20 +0100

    added 404 route

--- main.go
+++ main.go
@@ -12,6 +12,14 @@ func main() {
 	port := "8080"
 	router := mux.NewRouter()
 
+	// match all unknown routes
+	router.NotFoundHandler = http.HandlerFunc(notFound)
+
 	fmt.Println("Server is running on port:", port)
 	log.Fatalln(http.ListenAndServe(fmt.Sprintf("localhost:%s", port), router))
+}
+
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	http.Error(w, "404 - Page Not Found", http.StatusNotFound)
 }
